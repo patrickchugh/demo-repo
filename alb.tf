1bd609ddc0e750c55f0f718a9ea05ef0733c370b
patrickchugh <me@patrickchugh.com>
2026-03-14 10:49:06 +0000

    Updated with demo additions

--- alb.tf
+++ alb.tf
@@ -1,72 +1,7 @@
 # =============================================================================
-# WAF + Application Load Balancer
+# Application Load Balancer
 # =============================================================================
 
-# WAFv2 WebACL protecting the ALB
-resource "aws_wafv2_web_acl" "main" {
-  name  = "${var.project_name}-web-acl"
-  scope = "REGIONAL"
-
-  default_action {
-    allow {}
-  }
-
-  rule {
-    name     = "aws-managed-common-rules"
-    priority = 1
-
-    override_action {
-      none {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "CommonRules"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
-    name     = "rate-limit"
-    priority = 2
-
-    action {
-      block {}
-    }
-
-    statement {
-      rate_based_statement {
-        limit              = 2000
-        aggregate_key_type = "IP"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "RateLimit"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "WebACL"
-    sampled_requests_enabled   = true
-  }
-
-  tags = {
-    Name = "${var.project_name}-web-acl"
-  }
-}
-
-# Application Load Balancer
 resource "aws_lb" "main" {
   name               = "${var.project_name}-alb"
   internal           = false
@@ -77,12 +12,6 @@ resource "aws_lb" "main" {
   tags = {
     Name = "${var.project_name}-alb"
   }
-}
-
-# WAF association with ALB
-resource "aws_wafv2_web_acl_association" "main" {
-  resource_arn = aws_lb.main.arn
-  web_acl_arn  = aws_wafv2_web_acl.main.arn
 }
 
 # Target group for ECS
